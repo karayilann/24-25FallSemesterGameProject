0cf81b4ec4a4568eb2706f6ec424e2c690bb6c2f
Muhammet Hamit KARAYILAN <muhammethamitkarayilan@gmail.com>
2024-11-18 22:56:33 +0300

    Added Dissolve Shader

---
+++ MitologicalGame/Assets/_Project/Scripts/CoreScripts/Shader/ShaderSpecs.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+static class ShaderSpecs 
+{
+    
+    public static readonly int cutoff = Shader.PropertyToID("Cut off");
+    
+    
+}

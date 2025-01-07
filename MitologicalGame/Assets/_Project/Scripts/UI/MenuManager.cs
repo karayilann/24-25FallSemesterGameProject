96c1579dc75dd6a3a03f608a539c1e40b5d7d871
Muhammet Hamit KARAYILAN <muhammethamitkarayilan@gmail.com>
2025-01-07 23:56:25 +0300

    deleted comments

--- MitologicalGame/Assets/_Project/Scripts/UI/MenuManager.cs
+++ MitologicalGame/Assets/_Project/Scripts/UI/MenuManager.cs
@@ -2,9 +2,8 @@ using System;
 using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
-using UnityEngine.UI; // Değişen import satırı
+using UnityEngine.UI;
 using UnityEngine.Playables;
-using UnityEngine.Serialization;
 
 namespace _Project.Scripts.UI
 {
@@ -14,27 +13,23 @@ namespace _Project.Scripts.UI
 
         public PlayableDirector playableDirector;
         
-        [SerializeField] private float slideDistance = 1000f; // Ne kadar sola kayacağı
-        [SerializeField] private float animationDuration = 1f; // Animasyon süresi
-        [SerializeField] private float delayBetweenButtons = 0.1f; // Butonlar arası gecikme
+        [SerializeField] private float slideDistance = 1000f;
+        [SerializeField] private float animationDuration = 1f; 
+        [SerializeField] private float delayBetweenButtons = 0.1f;
         
         public void StartGame()
         {
-            // Her buton için sırayla animasyon başlat
             for (int i = 0; i < buttons.Count; i++)
             {
                 var button = buttons[i];
                 
-                // Her buton için gecikmeyi hesapla
                 float delay = i * delayBetweenButtons;
                 
-                // Butonu sola kaydır
                 button.transform.DOLocalMoveX(-slideDistance, animationDuration)
                     .SetDelay(delay)
                     .SetEase(Ease.InOutQuad);
             }
             
-            // Animasyonun ortasında PlayableDirector'ı çalıştır
             float middlePoint = (buttons.Count * delayBetweenButtons + animationDuration) / 2f;
             DOVirtual.DelayedCall(middlePoint, () => Debug.Log("Deneme"));
         }
